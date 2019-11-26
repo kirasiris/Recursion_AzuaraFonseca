c66d03f92d26e6dc0ab7bc90f50050fe286efa2c
Kevin Uriel Fonseca <kebin1421@hotmail.com>
2019-11-26 22:59:23 +0000

    Added a link where I got help to solve this program.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 /*
-Write a recursive function factorialR that will generate factorials based upon the user starting point (validated) input from the keyboard. i.e. When the user enters, 4, the function should multiply 4 x 3 x 2 x 1, and output the result of 24. 
+Write a recursive function factorialR that will generate factorials based upon the user starting point (validated) input from the keyboard. i.e. When the user enters, 4, the function should multiply 4 x 3 x 2 x 1, and output the result of 24.
+
+Credits:
+https://www.geeksforgeeks.org/recursion/
 */
 #include<iostream>
 #include "Input_Validation_Extended.h"
